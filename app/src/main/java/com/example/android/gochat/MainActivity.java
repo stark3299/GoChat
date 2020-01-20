2fed44e48525af7e7799ba048c4b945e28b7a8d7
Sumit Ranjan <ranjansumit430@gmail.com>
2020-01-20 14:56:48 +0530

    Splash

--- app/src/main/java/com/example/android/gochat/MainActivity.java
+++ app/src/main/java/com/example/android/gochat/MainActivity.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 1000;
+    private static int SPLASH_TIME_OUT = 1500;
+    public ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        logo = findViewById(R.id.logo);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -23,5 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         },SPLASH_TIME_OUT);
+        Animation myanim = Animation.loadAnimation(this,R.anim.animation);
+        logo.startAnimation(myanim);
     }
 }
