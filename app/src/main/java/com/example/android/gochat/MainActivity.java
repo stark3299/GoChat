b3d3f58ebaffc4db170f1aa6e9ab18f19896d335
Sumit Ranjan <ranjansumit430@gmail.com>
2020-01-20 14:39:33 +0530

    Splash

--- app/src/main/java/com/example/android/gochat/MainActivity.java
+++ app/src/main/java/com/example/android/gochat/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 100;
+    private static int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
